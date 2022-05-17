b8b67900821651c14fd453be0c0c04b53d2616be
Paul Rosania <paul.rosania@gmail.com>
2022-05-17 15:25:17 -0700

    remove unused lambda

--- packages/glider-api/src/extract.ts
+++
@@ -1,7 +0,0 @@
-import { SQSHandler } from 'aws-lambda';
-
-export const main: SQSHandler = async (event) => {
-  for (const record of event.Records) {
-    //console.log('Received record:', record);
-  }
-};
