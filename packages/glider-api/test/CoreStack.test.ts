6d942e09a4cecbc11eb1ea06f3bccce58ead9374
Paul Rosania <paul.rosania@gmail.com>
2022-05-17 15:57:50 -0700

    get tests working with repo-wide Jest

--- packages/glider-api/test/CoreStack.test.ts
+++ packages/glider-api/test/CoreStack.test.ts
@@ -1,13 +1,18 @@
-import * as sst from '@serverless-stack/resources';
+import { App, Stack } from '@serverless-stack/resources';
 import { Template } from 'aws-cdk-lib/assertions';
 
-import CoreStack from '../stacks/CoreStack';
+import { CoreStack } from '../stacks/CoreStack';
+
+// Help SST find our Lambdas. This is needed because we reference Lambdas
+// relative to this workspace, but Jest runs from the repository root.
+process.chdir('packages/glider-api');
 
 test('Core Stack', () => {
-  const app = new sst.App();
-  // WHEN
-  const stack = new CoreStack(app, 'test-stack');
+  const app = new App();
+  const stack = new Stack(app, 'test-stack');
+  CoreStack({ app, stack });
+
   // THEN
   const template = Template.fromStack(stack);
-  template.resourceCountIs('AWS::Lambda::Function', 18);
+  template.resourceCountIs('AWS::Lambda::Function', 17);
 });
